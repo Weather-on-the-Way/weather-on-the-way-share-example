13581a76936e1ceae671895124da1704c2f03059
Piotr Knapczyk <piotr.knapczyk@me.com>
2025-08-26 09:59:55 +0200

    Initial Commit

---
+++ ShareWithWeatherOnTheWayExample/ShareWithWeatherOnTheWayExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareWithWeatherOnTheWayExampleApp.swift
+//  ShareWithWeatherOnTheWayExample
+//
+//  Created by Piotr Knapczyk on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShareWithWeatherOnTheWayExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
